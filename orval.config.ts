cb2fdad7ee17d3ca025f71b0196437ac06689e92
Donkrzawayan <krzysztofdoniec8+github@gmail.com>
2026-01-15 23:20:28 +0100

    add TanStack Query

--- orval.config.ts
+++ orval.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
       mode: 'tags-split',
       target: 'src/api/generated',
       schemas: 'src/api/generated/model',
-      client: 'axios',
+      client: 'react-query',
       prettier: true,
       override: {
         mutator: {
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,25 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
